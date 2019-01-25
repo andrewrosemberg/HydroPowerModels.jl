3f62b8cfbddcc142a593fabdd969bb15f707c29e
andrewrosemberg <andrew@ele.puc-rio.br>
2019-01-25 13:33:13 -0200

    cascade enabled

--- src/constraint.jl
+++ src/constraint.jl
@@ -13,10 +13,10 @@ function rainfall_noises(sp, data::Dict, t::Int)
 end
 
 "creates hydro balance constraint"
-function constraint_hydro_balance(sp, data::Dict)
-    for i in 1:data["hydro"]["nHyd"]
+function constraint_hydro_balance(sp, data::Dict)    
+    for i in 2:data["hydro"]["nHyd"]
         @constraints(sp, begin
-            hydro_balance[i=1:data["hydro"]["nHyd"]], sp[:reservoir][i] - (sp[:reservoir0][i] - sp[:outflow][i] - sp[:spill][i]) == sp[:inflow][i]
+            hydro_balance[i=1:data["hydro"]["nHyd"]], sp[:reservoir][i] == sp[:reservoir0][i] + sp[:inflow][i] - sp[:outflow][i] - sp[:spill][i] + sum(sp[:outflow][j] + sp[:spill][j] for j in data["hydro"]["Hydrogenerators"][i]["upstrem_hydros"])
         end)
     end
 end
@@ -24,6 +24,6 @@ end
 "creates energy balance constraint"
 function constraint_hydro_generation(sp, data::Dict, pm::GenericPowerModel)
     @constraints(sp, begin
-        turbine_energy[i=1:data["hydro"]["nHyd"]], var(pm, 0, 1, :pg)[data["hydro"]["Hydrogenerators"][i]["index"]] == sp[:outflow][i]*data["hydro"]["Hydrogenerators"][i]["production_factor"]
+        turbine_energy[i=1:data["hydro"]["nHyd"]], var(pm, 0, 1, :pg)[data["hydro"]["Hydrogenerators"][i]["index_grid"]] == sp[:outflow][i]*data["hydro"]["Hydrogenerators"][i]["production_factor"]
     end)
 end
