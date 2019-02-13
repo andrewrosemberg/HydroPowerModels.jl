89cdd7e20ad3669aaef60cab23c105e42aeb7ef9
andrewrosemberg <andrew@ele.puc-rio.br>
2019-02-13 16:37:07 -0200

    update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -15,7 +15,8 @@ makedocs(
 )
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    deps   = Deps.pip("Tornado>=4.0.0,<5.0.0", "mkdocs",
+    "mkdocs-material" ,"python-markdown-math", "pygments", "pymdown-extensions"),
     repo   = "github.com/andrewrosemberg/HydroPowerModels.jl.git",
     julia = "0.6"
 )
