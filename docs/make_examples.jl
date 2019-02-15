57f90d6598a9abca1b0b0851df6f147113caa4db
andrewrosemberg <andrew@ele.puc-rio.br>
2019-02-15 16:25:18 -0200

    update make_examples

--- docs/make_examples.jl
+++ docs/make_examples.jl
@@ -1,4 +1,4 @@
 using Weave
 
-examples_dir = joinpath(dirname(dirname(@__FILE__)), "examples")
+examples_dir = joinpath(dirname(dirname(@__FILE__)), "examples/HydroValleys")
 weave(joinpath(examples_dir,"case3.jl"), out_path = "build/examples", doctype = "md2html")
