23e4185a354230dc2199de5cd534b0e6e3fb39a8
andrewrosemberg <andrew@ele.puc-rio.br>
2019-02-18 18:15:21 -0300

    rm weave case3 ac

--- docs/make_examples.jl
+++ docs/make_examples.jl
@@ -8,5 +8,3 @@ testcases_dir = joinpath(dirname(dirname(@__FILE__)), "testcases")
 
 weave(joinpath(examples_dir,"case3.jl"), out_path =joinpath(docs_dir,"build/examples"), doctype = "md2html", args=Dict(:testcases_dir=>testcases_dir))
 weave(joinpath(examples_dir,"case3_5years.jl"), out_path =joinpath(docs_dir,"build/examples"), doctype = "md2html", args=Dict(:testcases_dir=>testcases_dir))
-
-weave(joinpath(examples_dir,"case3_ac.jl"), out_path =joinpath(docs_dir,"build/examples"), doctype = "md2html", args=Dict(:testcases_dir=>testcases_dir))
